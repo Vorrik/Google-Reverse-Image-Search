1a1c3c31ba01c9b9e92d2c3b68b4efbe380dd923
Vorrik <moderatorxd66@gmail.com>
2024-06-23 04:59:46 +0300

    added switch_safe_mode method

--- src/google_img_source_search/reverse_image_searcher.py
+++ src/google_img_source_search/reverse_image_searcher.py
@@ -1,15 +1,20 @@
+import re
 from requests import Session
 
-from .google_items.image import Image
 from .google_items.search_item import SearchItem
-from .f_req_template import build_f_req
-from .api_response_parser import ApiResponseParser
+from .safe_modes import SafeMode
+from .exceptions import SafeModeSwitchError
+
+from .image_uploader import ImageUploader
+from .image_source_searcher import ImageSourceSearcher
 
 
 class ReverseImageSearcher:
-    def __init__(self):
-        self.url = 'https://lens.google.com'
-        self.session = Session()
+    def __init__(self, session=None, image_uploader=None, image_source_searcher=None):
+        self.session = session or Session()
+        self.image_uploader = image_uploader or ImageUploader(self.session)
+        self.image_source_searcher = image_source_searcher or ImageSourceSearcher(self.session)
+
         self.session.headers.update(
             {'User-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0'}
         )
@@ -17,32 +22,24 @@ class ReverseImageSearcher:
             'response': lambda r, *args, **kwargs: r.raise_for_status()
         }
 
-    def upload_image(self, image_url: str) -> Image:
-        upload_response = self.session.get(f'{self.url}/uploadbyurl', params={'url': image_url}, allow_redirects=True)
-        return ApiResponseParser.extract_image(upload_response.text)
+    def switch_safe_mode(self, safe_mode: SafeMode):
+        safe_search_response = self.session.get('https://google.com/safesearch')
 
-    def search_image_src(self, image: Image) -> list[SearchItem]:
-        params = {
-            'soc-app': '1',
-            'soc-platform': '1',
-            'soc-device': '1',
-            'rt': 'c',
-        }
+        switch_attr = {SafeMode.DISABLED: 'data-setprefs-off-url',
+                       SafeMode.BLUR: 'data-setprefs-blur-url',
+                       SafeMode.FILTER: 'data-setprefs-filter-url'}[safe_mode]
 
-        data = {
-            'f.req': build_f_req(image.id_1, image.id_2)
-        }
+        switch_params = re.search(rf'(?<={switch_attr}=").*?(?=")', safe_search_response.text).group(0)
+        switch_response = self.session.get(f'https://google.com{switch_params.replace("amp;", "")}')
 
-        src_response = self.session.post(f'{self.url}/_/LensWebStandaloneUi/data/batchexecute',
-                                         params=params, data=data, allow_redirects=True)
-
-        return ApiResponseParser.extract_image_src(src_response.text)
+        if switch_response.status_code != 204:
+            raise SafeModeSwitchError()
 
     def search(self, image_url: str) -> list[SearchItem]:
         """
-        Searches for image sources by URL using google services
-        :return: list of search items
+        Searches for web pages using the specified image
+        :return: list of search items (page url, title, image url)
         """
 
-        google_image = self.upload_image(image_url)
-        return self.search_image_src(google_image)
+        google_image = self.image_uploader.upload(image_url)
+        return self.image_source_searcher.search(google_image)
