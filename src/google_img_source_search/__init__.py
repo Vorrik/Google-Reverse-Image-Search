7e967c2557c8abdda1fb1306ce9f792182d565c3
Vorrik <moderatorxd66@gmail.com>
2024-06-24 16:03:38 +0300

    updated exceptions

--- src/google_img_source_search/__init__.py
+++ src/google_img_source_search/__init__.py
@@ -1,3 +1,3 @@
 from .reverse_image_searcher import ReverseImageSearcher
 from .safe_modes import SafeMode
-from .exceptions import GoogleImageSearcherError, InvalidImageURL, SafeModeSwitchError
+from .exceptions import *
